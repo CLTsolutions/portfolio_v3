39cd581675788d522ec277a2334e35ccd16a4e4a
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2023-09-30 17:05:48 -0400

    chore: rm inter and clean up readme

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,4 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: "Chelsey Tschida's Portfolio",
@@ -11,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="synthwave">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
