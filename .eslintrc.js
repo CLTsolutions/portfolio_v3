14d23de2a6c3f51769d8fbf4a2d43144f4f2e0a4
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2023-12-01 11:55:48 -0500

    feat: add simple import sort plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,26 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'airbnb-base', 'prettier'],
+  plugins: ['simple-import-sort'],
   rules: {
     'no-unused-vars': 'warn',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          // Packages `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          ['^(@|components)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.?(css)$'],
+        ],
+      },
+    ],
   },
 };
